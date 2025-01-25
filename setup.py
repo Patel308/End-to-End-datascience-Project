97d6269829183b8f592527abf93c7ba5cfa9d0f5
deepesh <techiedeepeshpatel@gmail.com>
2025-01-25 13:08:02 +0530

    project setups

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List 
+
+hypen_e_dot = '-e .'
+
+def get_requirements(path:str)->list[str]:
+    requirements = []
+    with open(path, 'rb') as obj:
+        requirements =  obj.readline()
+        requirements =  [req.replace('\n', '') for req in requirements]
+        if hypen_e_dot in requirements:
+            requirements.remove(hypen_e_dot)
+
+    return requirements        
+
+
+setup(
+    name='MlProject',
+    version = '0.1',
+    author='Deepesh Patel'
+    author_email="techiedeepeshpatel@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
