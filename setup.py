4eb1b0125dcb9ccd760cc24bd5312585bbaa6be8
deepesh <techiedeepeshpatel@gmail.com>
2025-01-25 13:34:10 +0530

    some changes

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from typing import List
 
 hypen_e_dot = '-e .'
 
-def get_requirements(path:str)->list[str]:
+def get_requirements(path:str)->List[str]:
     requirements = []
-    with open(path, 'rb') as obj:
-        requirements =  obj.readline()
+    with open(path) as obj:
+        requirements =  obj.readlines()
         requirements =  [req.replace('\n', '') for req in requirements]
         if hypen_e_dot in requirements:
             requirements.remove(hypen_e_dot)
@@ -17,7 +17,7 @@ def get_requirements(path:str)->list[str]:
 setup(
     name='MlProject',
     version = '0.1',
-    author='Deepesh Patel'
+    author='Deepesh Patel',
     author_email="techiedeepeshpatel@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
